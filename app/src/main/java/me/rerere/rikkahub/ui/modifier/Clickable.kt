6980cf4e27f76e55f11d452b32713967fedd6aa9
Inonvation <1829426594@qq.com>
2026-04-30 14:15:04 +0800

    更新 Clickable.kt

--- app/src/main/java/me/rerere/rikkahub/ui/modifier/Clickable.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/modifier/Clickable.kt
@@ -6,15 +6,23 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.semantics.Role
 
 @Composable
 fun Modifier.onClick(
     enabled: Boolean = true,
     onClick: () -> Unit
-): Modifier = this.then(Modifier.clickable(
-    onClick = onClick,
-    interactionSource = remember { MutableInteractionSource() },
-    indication = LocalIndication.current,
-    role = Role.Button,
-))
+): Modifier {
+    val hapticFeedback = LocalHapticFeedback.current
+    return this.then(Modifier.clickable(
+        onClick = {
+            hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+            onClick()
+        },
+        interactionSource = remember { MutableInteractionSource() },
+        indication = LocalIndication.current,
+        role = Role.Button,
+    ))
+}
