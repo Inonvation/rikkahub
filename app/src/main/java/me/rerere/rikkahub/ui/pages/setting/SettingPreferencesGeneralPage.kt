e4762e20f2f0ffb73493dd5a421956cca03d4e7a
Inonvation <1829426594@qq.com>
2026-07-31 15:40:43 +0800

    chore: 补交所有遗留改动

--- app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingPreferencesGeneralPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingPreferencesGeneralPage.kt
@@ -195,6 +195,19 @@ fun SettingPreferencesGeneralPage(vm: SettingVM = koinViewModel()) {
                         },
                     )
                     item(
+                        headlineContent = { Text(stringResource(R.string.setting_display_page_enable_message_generation_started_haptic_effect_title)) },
+                        supportingContent = { Text(stringResource(R.string.setting_display_page_enable_message_generation_started_haptic_effect_desc)) },
+                        trailingContent = {
+                            Switch(
+                                checked = displaySetting.enableMessageGenerationStartedHapticEffect,
+                                enabled = displaySetting.enableHapticFeedback,
+                                onCheckedChange = {
+                                    updateDisplaySetting(displaySetting.copy(enableMessageGenerationStartedHapticEffect = it))
+                                }
+                            )
+                        },
+                    )
+                    item(
                         headlineContent = { Text(stringResource(R.string.setting_display_page_enable_message_generation_haptic_effect_title)) },
                         supportingContent = { Text(stringResource(R.string.setting_display_page_enable_message_generation_haptic_effect_desc)) },
                         trailingContent = {
