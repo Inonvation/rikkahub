a726c688e77cfb1787c156b19b072c9c44e6db02
Inonvation <1829426594@qq.com>
2026-08-06 01:17:22 +0800

    chore: bump version to 2.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.inonvation.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 253
-        versionName = "2.5.2"
+        versionCode = 254
+        versionName = "2.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
