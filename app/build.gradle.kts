def1247c0ccf4979fdf758e6f4c50482ad775139
Inonvation <1829426594@qq.com>
2026-09-05 21:44:25 +0800

    chore: bump version 2.12.1 (267)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.inonvation.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 266
-        versionName = "2.12.0"
+        versionCode = 267
+        versionName = "2.12.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
